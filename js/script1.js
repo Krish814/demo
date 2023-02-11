a77468cc1806f6c107e3ab80adc57f30014861a2
Krish Panchani <68278137+Krish814@users.noreply.github.com>
2023-02-11 15:56:04 -0500

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,22 @@
+let navbar = document.querySelector('.navbarWeb');
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+searchBtn.addEventListener('click', () => {
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+});
+
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
